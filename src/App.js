6b03523b227f615f86f45b3621049d1bf871db8f
StephensGit <stephenmoran@live.ie>
2020-11-11 16:39:19 +0000

    Finished setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 class App extends Component {
   render(){
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
     return (
       <Fragment className="App">
-        <h1>Hello</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </Fragment>
     );
   }
