76c5eb9cf1dba23c17b1bef0d34cf44f430b5c7f
StephensGit <stephenmoran@live.ie>
2020-11-11 17:07:56 +0000

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render(){
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
-
     return (
-      <Fragment className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-      </Fragment>
+      <div className="App">
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        
+      </div>
     );
   }
 }
