1d2c8c6f7b7fc9a5f58a923468b28f2b5b90ed64
StephensGit <stephenmoran@live.ie>
2020-11-11 16:24:38 +0000

    First commit, create react app - clean up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment, Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <Fragment className="App">
+        <h1>Hello</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
