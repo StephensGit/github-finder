1c9c8cb25c9e7b79131fe4f6fa74bdfe51603e90
StephensGit <stephenmoran@live.ie>
2020-11-11 20:23:17 +0000

    added http request to githup api

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class User extends Component {
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
         return (
             <div className="card text-center">
                 <img 
@@ -20,7 +18,9 @@ class User extends Component {
                 </div>
             </div>
         )
-    }
 }
 
-export default User
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+}
+export default UserItem
