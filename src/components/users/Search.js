e79514901632e8992a19b2d8051d6ff101c5fda2
StephensGit <stephenmoran@live.ie>
2020-11-18 17:15:16 +0000

    Added a component/ button to clear the users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ export class Search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
+        clearUsers:  PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     }
 
     onSubmit = (e) => {
@@ -24,6 +26,7 @@ export class Search extends Component {
     }
 
     render() {
+        const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -36,6 +39,15 @@ export class Search extends Component {
                     />
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                    <button 
+                        className="btn btn-light btn-block" 
+                        onClick={clearUsers}
+                    >
+                        Clear
+                    </button>
+                )}
+
             </div>
         )
     }
