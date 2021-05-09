a29aad2004739942232b0acf12ccc9a1b687c412
StephensGit <stephenmoran@live.ie>
2021-05-09 22:07:13 +0100

    Added Repos, repos Item component & data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import About from './components/pages/About';
+import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
@@ -9,8 +12,10 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
 
   // async componentDidMount() {
@@ -40,6 +45,30 @@ class App extends Component {
     console.log(this.state.users);
   }
 
+  //  Search Single User
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  }
+
+  //  Search Single User
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   clearUsers = () => {
     this.setState({ users: [], loading: false })
   }
@@ -54,21 +83,44 @@ class App extends Component {
   }
 
   render(){
-    const { users, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
-      <div className="App">
-        <Navbar title='Github Finder' icon='fab fa-github' />
-        <div className='container'>
-          <Alert alert={this.state.alert}/>
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert alert={this.state.alert}/>
+
+              <Switch>
+                <Route exact path='/' render={props => (
+                  <Fragment>
+                    <Search 
+                      searchUsers={this.searchUsers} 
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+                />
+                <Route exact path='/user/:login' render={props=> (
+                  <User 
+                    {...props} 
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos} 
+                    user={user} 
+                    repos={repos}
+                    loading={loading} 
+                  />
+                )} />
+                <Route exact path='/about' component={About} />
+
+              </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
+
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About</h1>
+            <p>App</p>
+            <p>version 1.1</p>
+        </Fragment>
+    )
+}
+
+export default About
+  
